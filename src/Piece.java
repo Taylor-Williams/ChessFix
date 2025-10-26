25e3d9792db4522b67c18f430be131e7fd71207d
Taylor Williams <taylor@thewilliams.net>
2025-10-25 17:35:42 -0700

    convert from abstract class to interface

--- src/Piece.java
+++ src/Piece.java
@@ -3,42 +3,18 @@ package chess;
 import java.util.List;
 import java.util.ArrayList;
 
-public abstract class Piece {
-    protected Color color;
-    protected Position position;
+public interface Piece {
     
-    public Piece(Color color, Position position) {
-        this.color = color;
-        this.position = position;
-    }
+    public Color getColor() ;
     
-    public Color getColor() {
-        return color;
-    }
+    public Position getPosition() ;
     
-    public Position getPosition() {
-        return position;
-    }
-    
-    public void setPosition(Position position) {
-        this.position = position;
-    }
+    public void setPosition(Position position) ;
     
     public abstract String getSymbol();
     
     public abstract List<Position> getValidMoves(Board board);
     
-    protected List<Position> filterValidMoves(List<Position> potentialMoves, Board board) {
-        List<Position> validMoves = new ArrayList<>();
-        for (Position move : potentialMoves) {
-            if (move.isValid()) {
-                Piece targetPiece = board.getPiece(move);
-                if (targetPiece == null || targetPiece.getColor() != this.color) {
-                    validMoves.add(move);
-                }
-            }
-        }
-        return validMoves;
-    }
+    public List<Position> filterValidMoves(List<Position> potentialMoves, Board board);
 }
 

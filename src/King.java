f9e9a14aab742f82bfd93e82014bec9f9e34180d
Taylor Williams <taylor@thewilliams.net>
2025-10-25 17:44:26 -0700

    convert King to implement Piece

--- src/King.java
+++ src/King.java
@@ -3,18 +3,28 @@ package chess;
 import java.util.List;
 import java.util.ArrayList;
 
-public class King extends Piece {
+public class King implements Piece {
     
     public King(Color color, Position position) {
         super(color, position);
     }
+
+    public Color getColor() {
+        return color;
+    }
     
-    @Override
+    public Position getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    
     public String getSymbol() {
         return color == Color.WHITE ? "♔" : "♚";
     }
     
-    @Override
     public List<Position> getValidMoves(Board board) {
         List<Position> moves = new ArrayList<>();
         
@@ -36,6 +46,19 @@ public class King extends Piece {
         }
         
         return moves;
+    }
+
+    public List<Position> filterValidMoves(List<Position> potentialMoves, Board board) {
+        List<Position> validMoves = new ArrayList<>();
+        for (Position move : potentialMoves) {
+            if (move.isValid()) {
+                Piece targetPiece = board.getPiece(move);
+                if (targetPiece == null || targetPiece.getColor() != this.color) {
+                    validMoves.add(move);
+                }
+            }
+        }
+        return validMoves;
     }
 }
 
